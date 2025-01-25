6873bfd5b40d2990a106f5ea101a8c3584aea86b
Gutif <165393961+Gutif@users.noreply.github.com>
2025-01-25 11:27:11 +0300

    Update to close while clicking outside screen script.js

--- JS/script.js
+++ JS/script.js
@@ -9,6 +9,15 @@ toggleBtn.onclick = function () {
   toggleBtnIcon.className = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
 
+// Add this event listener to the document
+document.addEventListener("click", function (event) {
+  // Check if the click happened outside the dropdown menu and the toggle button
+  if (!dropDownMenu.contains(event.target) && !toggleBtn.contains(event.target)) {
+    dropDownMenu.classList.remove("open");
+    toggleBtnIcon.className = "fa-solid fa-bars"; 
+  }
+});
+
 document.getElementById("myTextarea").addEventListener("input", function () {
   document.getElementById("submitButton").disabled = this.value.trim() === "";
 });
